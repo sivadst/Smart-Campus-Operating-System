dd16ef26931494e15a10fade7e9a9fb0c033166f
Smart Campus Developer <developer@smartcampus.com>
2026-08-25 22:03:15 +0530

    feat: add AuthResponse DTO for authentication token response

---
+++ src/main/java/com/campus/smartcampus/dto/response/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.campus.smartcampus.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+}
