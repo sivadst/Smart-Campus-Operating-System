e4819cf15fbe0d66498843e51da913cc43467b89
Smart Campus Developer <developer@smartcampus.com>
2026-08-31 22:48:51 +0530

    feat: add UserUpdateRequest DTO for admin user profile updates

---
+++ src/main/java/com/campus/smartcampus/dto/request/UserUpdateRequest.java
@@ -0,0 +1,17 @@
+﻿package com.campus.smartcampus.dto.request;
+
+import com.campus.smartcampus.enums.UserRole;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateRequest {
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private UserRole role;
+    private Boolean isActive;
+    private Boolean emailVerified;
+}
