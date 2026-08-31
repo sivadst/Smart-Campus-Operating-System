418279f4bdad18e62e453c62face886c7b4b0398
Smart Campus Developer <developer@smartcampus.com>
2026-08-31 22:16:16 +0530

    feat: add DepartmentRequest DTO for department creation input

---
+++ src/main/java/com/campus/smartcampus/dto/request/DepartmentRequest.java
@@ -0,0 +1,19 @@
+﻿package com.campus.smartcampus.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentRequest {
+    @NotBlank(message = "Department name is required")
+    private String name;
+
+    @NotBlank(message = "Department code is required")
+    private String code;
+
+    private String description;
+    private String headOfDepartment;
+}
