08df10d32c6ab86a9a9d928ddcd6d33e4159bb9d
Smart Campus Developer <developer@smartcampus.com>
2026-08-31 22:16:12 +0530

    feat: add NoticeCategory enum for campus announcement classification

---
+++ src/main/java/com/campus/smartcampus/enums/NoticeCategory.java
@@ -0,0 +1,13 @@
+﻿package com.campus.smartcampus.enums;
+
+public enum NoticeCategory {
+    ACADEMIC,
+    ADMINISTRATIVE,
+    EVENT,
+    EXAMINATION,
+    PLACEMENT,
+    SPORTS,
+    CULTURAL,
+    EMERGENCY,
+    GENERAL
+}
