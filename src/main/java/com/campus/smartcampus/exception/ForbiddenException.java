76fc79700f3f3acccbd56ac5ac7095bd6dc35042
Smart Campus Developer <developer@smartcampus.com>
2026-08-31 22:16:19 +0530

    feat: add ForbiddenException for 403 access denied handling

---
+++ src/main/java/com/campus/smartcampus/exception/ForbiddenException.java
@@ -0,0 +1,9 @@
+﻿package com.campus.smartcampus.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ForbiddenException extends CampusException {
+    public ForbiddenException(String message) {
+        super(message, "FORBIDDEN", HttpStatus.FORBIDDEN);
+    }
+}
