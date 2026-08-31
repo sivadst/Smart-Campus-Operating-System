12c648b78d58b5dc70f91f61881b93a03d41eaba
Smart Campus Developer <developer@smartcampus.com>
2026-08-31 22:16:12 +0530

    feat: add RoomType enum for campus facility categorization

---
+++ src/main/java/com/campus/smartcampus/enums/RoomType.java
@@ -0,0 +1,15 @@
+﻿package com.campus.smartcampus.enums;
+
+public enum RoomType {
+    LECTURE_HALL,
+    LABORATORY,
+    SEMINAR_ROOM,
+    LIBRARY,
+    AUDITORIUM,
+    COMPUTER_LAB,
+    CONFERENCE_ROOM,
+    OFFICE,
+    CAFETERIA,
+    GYMNASIUM,
+    WORKSHOP
+}
