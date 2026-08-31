97d55532cc0ac68920517241421df59bf8409fda
Smart Campus Developer <developer@smartcampus.com>
2026-08-31 22:57:29 +0530

    feat: add BookCategory enum for library classification

---
+++ src/main/java/com/campus/smartcampus/enums/BookCategory.java
@@ -0,0 +1,14 @@
+﻿package com.campus.smartcampus.enums;
+
+public enum BookCategory {
+    COMPUTER_SCIENCE,
+    INFORMATION_TECHNOLOGY,
+    ELECTRONICS,
+    MECHANICAL,
+    CIVIL,
+    MATHEMATICS,
+    PHYSICS,
+    CHEMISTRY,
+    BUSINESS_MANAGEMENT,
+    GENERAL_LITERATURE
+}
