8cad6aa9d29e4087e7ea3bb534195aefea375b12
Smart Campus Developer <developer@smartcampus.com>
2026-08-31 22:57:27 +0530

    feat: add NotificationType enum for multi-channel notifications

---
+++ src/main/java/com/campus/smartcampus/enums/NotificationType.java
@@ -0,0 +1,10 @@
+﻿package com.campus.smartcampus.enums;
+
+public enum NotificationType {
+    SYSTEM,
+    NOTICE,
+    BOOKING_UPDATE,
+    ATTENDANCE_WARNING,
+    EVENT_REMINDER,
+    LIBRARY_DUE
+}
