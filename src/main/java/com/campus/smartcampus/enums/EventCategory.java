56492fe59f22f1ba72bad46eb5604b96c8dac6ee
Smart Campus Developer <developer@smartcampus.com>
2026-08-31 22:57:32 +0530

    feat: add EventCategory enum for campus activities

---
+++ src/main/java/com/campus/smartcampus/enums/EventCategory.java
@@ -0,0 +1,12 @@
+﻿package com.campus.smartcampus.enums;
+
+public enum EventCategory {
+    HACKATHON,
+    WORKSHOP,
+    SEMINAR,
+    GUEST_LECTURE,
+    CULTURAL_FEST,
+    SPORTS_MEET,
+    CAREER_FAIR,
+    ORIENTATION
+}
