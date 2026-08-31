190f441db61fd4bf9a2f9e104462ff19e46341d2
Smart Campus Developer <developer@smartcampus.com>
2026-08-31 22:48:49 +0530

    feat: add BuildingRequest DTO for building creation input

---
+++ src/main/java/com/campus/smartcampus/dto/request/BuildingRequest.java
@@ -0,0 +1,25 @@
+﻿package com.campus.smartcampus.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BuildingRequest {
+    @NotBlank(message = "Building name is required")
+    private String name;
+
+    @NotBlank(message = "Building code is required")
+    private String code;
+
+    private String address;
+
+    @Min(value = 1, message = "Total floors must be at least 1")
+    private int totalFloors;
+
+    private Double latitude;
+    private Double longitude;
+}
