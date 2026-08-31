a93c5f0d3546898d92ea4fc4d49ae9f89e4dfa87
Smart Campus Developer <developer@smartcampus.com>
2026-08-31 22:57:27 +0530

    feat: add UserRegisteredEvent domain event

---
+++ src/main/java/com/campus/smartcampus/event/UserRegisteredEvent.java
@@ -0,0 +1,20 @@
+﻿package com.campus.smartcampus.event;
+
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+import java.util.UUID;
+
+@Getter
+public class UserRegisteredEvent extends ApplicationEvent {
+    private final UUID userId;
+    private final String email;
+    private final String fullName;
+
+    public UserRegisteredEvent(Object source, UUID userId, String email, String fullName) {
+        super(source);
+        this.userId = userId;
+        this.email = email;
+        this.fullName = fullName;
+    }
+}
