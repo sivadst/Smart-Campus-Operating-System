0d45a0dbbfd6b3478a2a50656a4d998799611d74
Smart Campus Developer <developer@smartcampus.com>
2026-08-31 22:16:20 +0530

    feat: add BadRequestException for 400 invalid request handling

---
+++ src/main/java/com/campus/smartcampus/exception/BadRequestException.java
@@ -0,0 +1,9 @@
+﻿package com.campus.smartcampus.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BadRequestException extends CampusException {
+    public BadRequestException(String message) {
+        super(message, "BAD_REQUEST", HttpStatus.BAD_REQUEST);
+    }
+}
